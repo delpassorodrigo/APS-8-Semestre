7005dcd98123fb8775ea68e47c81ea3cfed87901
Rodrigo Delpasso <delpassorodrigo@gmail.com>
2023-11-11 18:35:53 -0300

    feat: Implementação Google oAuth2

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, redirect, session
+from flask import Flask, render_template, url_for, redirect, session, abort
 import requests
 import json
 import datetime as dt
+from authlib.integrations.flask_client import OAuth
 from geolite2 import geolite2
 from pyowm import OWM
 from pyowm.utils import config
@@ -9,6 +10,28 @@ from pyowm.utils import timestamps
 from unidecode import unidecode
 
 app = Flask(__name__)
+
+appConf = {
+    "OAUTH2_CLIENT_ID": "564598204141-cmcgu790m0mq4jnaj1hna2cqj0g5k7kt.apps.googleusercontent.com",
+    "OAUTH2_CLIENT_SECRET": "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",
+    "OAUTH2_META_URL": "https://accounts.google.com/.well-known/openid-configuration",
+    "FLASK_SECRET": "ec16d517-c992-4697-a1f3-df5346b321a3",
+    "FLASK_PORT": 5000
+}
+
+app.secret_key = appConf.get("FLASK_SECRET")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "GoogleOAuth",
+    client_id=appConf.get("OAUTH2_CLIENT_ID"),
+    client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/user.gender.read",
+    },
+    server_metadata_url=f'{appConf.get("OAUTH2_META_URL")}',
+)
 
 # Função responsável por retornar a latitude e a longitude de uma determinada cidade
 def get_lat_lon(city_name):
@@ -68,7 +91,32 @@ def homepage():
     pol_mgr = owm.airpollution_manager()
     air_status = pol_mgr.air_quality_at_coords(latitude, longitude) 
 
-    return render_template('index.html', ip=ip, date=date, location=location, clima = clima, poluentes=air_status, email=email)
+    return render_template('index.html', ip=ip, date=date, location=location, clima = clima, poluentes=air_status, session=session.get("user"),
+                           pretty=json.dumps(session.get("user"), indent=4))
+
+@app.route("/signin-google")
+def googleCallback():
+    token = oauth.GoogleOAuth.authorize_access_token()
+    personDataUrl = "https://people.googleapis.com/v1/people/me?personFields=genders,birthdays"
+    personData = requests.get(personDataUrl, headers={
+        "Authorization": f"Bearer {token['access_token']}"
+    }).json()
+    token["personData"] = personData
+    session["user"] = token
+    return redirect(url_for("homepage"))
+
+
+@app.route("/google-login")
+def googleLogin():
+    if "user" in session:
+        abort(404)
+    return oauth.GoogleOAuth.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("homepage"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=appConf.get("FLASK_PORT"), debug=True)
